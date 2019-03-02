136677e1c45676b208c26464de18eb8c09d5bc68
Anastasia <atamantseva2016@ya.ru>
2019-03-02 22:00:36 +0300

    first commit Extended-Syntax

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -11,8 +11,27 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+    "use strict"
+
+let diskriminant = (b ** 2) - (4 * a * c);
+
+let x;
+let x1;
+let x2;
+let d = Math.sqrt(diskriminant)
+
+if (diskriminant < 0) {
+  console.log("Корней уравнения нет");
+} else if (diskriminant = 0) {
+  x = (-b) / (2 * a);
+  console.log('Корень уравнения= ' + x)
+} else {
+  x1 = (-b + d) / 2 * a;
+  x2 = (-b - d) / 2 * a;
+  x = [x1, x2]
+  console.log('Корни уравнения ' + x);
+}
+return x
 }
 
 function calculateDrinkTask(){
@@ -23,9 +42,19 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №2 писать здесь
-    //console.log(result)
-    //return result;
+let now = new Date();
+let age = now.getFullYear() - dateOfBirthday.getFullYear();
+
+switch (true) {
+case age > 18: 
+  result = `Не желаете ли олд-фэшн, ${name}?`;
+  break;
+default:
+ result=`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+ break;
+}
+console.log(result);
+return result;
 }
 
 function calculateAverageRating(){
@@ -35,6 +64,21 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
-    // код для задачи №3 писать здесь
-    //return averageMark;
+/*Проверка количества оценок, если оценок больше 5-ти, то выводится количество оценок у ученика
+а затем длина массива обрезается до 5-ти*/
+   console.log(marks.length);
+   if (marks.length > 5) {
+      console.log(`У ученика ${marks.length} оценок`)  
+      marks.length = 5  
+   }
+
+let sum = 0;
+for(i = 0; i < marks.length; i++){
+    sum +=marks[i];
+    }
+
+let averageMark = sum/marks.length
+console.log(averageMark)
+
+return averageMark
 }
