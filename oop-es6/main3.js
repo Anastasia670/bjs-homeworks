b1bce1adf631b859a68f6f7e86bf211a7c70869d
Anastasia <atamantseva2016@ya.ru>
2019-04-08 22:37:26 +0300

    ЗАДАНИЕ 3

---
+++ oop-es6/main3.js
@@ -0,0 +1,56 @@
+'use strict'  
+class StudentLog {
+  constructor(name) {
+    this.name = name
+    this.marks = {} 
+    
+  }
+  getName() {
+    return this.name    
+  }
+addGrade( grade, subject ){
+   if (this.marks[subject] === undefined){
+   this.marks[subject] = [];
+        };  
+  if (grade > 5 || grade < 1 || (typeof grade !== 'number') || isNaN (parseInt(grade)))  {
+    console.log (`Вы пытались поставить оценку ${grade} по предмету ${subject} Допустимый предел: 0-5`)
+  }
+  else{
+    this.marks[subject].push(grade);
+          }
+    return this.marks[subject];
+ }
+
+ getAverageBySubject( subject ){
+   if (this.marks[subject] === undefined) {
+   return 0
+ } else{
+   let sum = 0
+   for (let i = 0; i < this.marks[subject].length; i ++){
+    sum += this.marks[subject][i] 
+   }
+   let average = sum / this.marks[subject].length;
+   return average    
+    }
+ }
+getTotalAverage(){
+ if (this.marks === undefined) {
+   return 0
+ } else{
+let total = 0
+let average = 0
+for(let value in this.marks) {  
+  total += this.getAverageBySubject( value )
+ }
+  return total / Object.keys(this.marks).length
+ }
+ 
+}
+  }
+const log = new StudentLog( 'Игорь Иванов' );
+console.log( log.getName() );
+console.log( log.addGrade( 5, 'algebra'));
+console.log( log.addGrade( 4, 'algebra'));
+console.log( log.addGrade( 4, 'english'))
+console.log( log.getAverageBySubject( 'algebra' ));
+console.log (log.getTotalAverage())
